7200518a957eec7ab4ef76763724bddff01608e7
arron.zhao <aaron.zhao>
2022-07-22 10:54:01 +0800

    add study

--- test/data/point_test.go
+++ test/data/point_test.go
@@ -1,6 +1,9 @@
 package data
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // 指针
 // 基本数据类型 变量存的就是值，也叫值类型
@@ -8,6 +11,8 @@ import "testing"
 // 指针类型 变量存的是一个地址 这个地址指向的空间存的才是值
 // 获取指针类型所指向的值，使用 *
 
+// 值类型 ： int float bool string 数组 struct  数据在栈区
+// 引用类型： 指针 slice切片 map 管道chan interface 数据在堆区
 func TestPoint(t *testing.T) {
 	a := 1
 	aPtr := &a
@@ -17,7 +22,22 @@ func TestPoint(t *testing.T) {
 
 func TestPoint2(t *testing.T) {
 	var num int
-	var ptr *int = &num
+	var ptr *int = &num // 指针变量
+	fmt.Println("num的地址:", &num)
+	fmt.Println("ptr本身的地址：", &ptr)
+	// ptr存储的值是num的内存地址
+	fmt.Println("ptr存储的值为：", ptr)
 	t.Log(ptr, &ptr, *ptr)
+}
+
+func TestPoint3(t *testing.T) {
+	var num int = 9
+	fmt.Printf("num的address：%v\n", &num)
+
+	var ptr *int = &num
+
+	*ptr = 10 // 这里修改会关联到 num
+
+	fmt.Println("num:", num)
 
 }
