e9c24164890f1be86b0950d47f8cb99c36093598
aaron.zhao <aaron.zhao@voyageone.cn>
2022-09-17 16:08:26 +0800

    add test

---
+++ test/func/for_test.go
@@ -0,0 +1,50 @@
+package _func
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestForFunc(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("hello world")
+	}
+}
+
+func TestForFunc2(t *testing.T) {
+	i := 0
+	for i < 10 {
+		fmt.Println("hello world")
+		i++
+	}
+}
+
+func TestForString(t *testing.T) {
+	var str string = "hello,world"
+	for i := 0; i < len(str); i++ {
+		fmt.Printf("%c---%v\n", str[i], str[i])
+	}
+}
+
+func TestForString2(t *testing.T) {
+	str := "hello,world"
+	for index, val := range str {
+		fmt.Printf("index=%d,value=%c\n", index, val)
+	}
+}
+
+func TestForString3(t *testing.T) {
+	str := "hello,北京"
+	newStr := []rune(str)
+	for i := 0; i < len(newStr); i++ {
+		fmt.Printf("%c---%v\n", newStr[i], newStr[i])
+	}
+}
+
+// rang是按字节进行遍历 推荐使用
+func TestForString4(t *testing.T) {
+	str := "hello,上海"
+	for index, val := range str {
+		fmt.Printf("index=%d,value=%c\n", index, val)
+	}
+}
---
+++ test/func/switch_test.go
@@ -0,0 +1,41 @@
+package _func
+
+import (
+	"fmt"
+	"go/types"
+	"testing"
+)
+
+func TestSwitchFunc(t *testing.T) {
+	var value byte
+	fmt.Println("请输入a,b,c,d,e,f")
+	fmt.Scanf("%c", &value)
+
+	switch value {
+	case 'a':
+		fmt.Println("aaaaaa")
+	case 'b':
+		fmt.Println("b")
+	default:
+		fmt.Println("输入错误")
+	}
+}
+
+var x interface{}
+
+func TestSwitchInterface(t *testing.T) {
+	var y = 10.0
+	x = y
+	switch i := x.(type) {
+	case types.Nil:
+		fmt.Printf("x的类型为：%T", i)
+	case float64:
+		fmt.Printf("x的类型是float64")
+	case int:
+		fmt.Printf("x的类型是int")
+	case func(int2 int) float64:
+		fmt.Println("x 是func(int)类型")
+	default:
+		fmt.Println("未知性")
+	}
+}
